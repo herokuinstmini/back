3f7e9e9c18060fc3ff422519a510c3580fc9b434
TikhonovaElena <lenatika0@gmail.com>
2021-06-23 21:32:32 +0300

    return whitenoise to settings

--- el/settings.py
+++ el/settings.py
@@ -156,12 +156,10 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
-
-# django_heroku.settings(locals())
