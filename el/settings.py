0987d4aa727bdcb778e74ac50a4c1ace03196fbe
TikhonovaElena <lenatika0@gmail.com>
2021-06-23 16:51:40 +0300

    change settings and requirements

--- el/settings.py
+++ el/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SK
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.133']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.133', '*']
 
 
 # Application definition
